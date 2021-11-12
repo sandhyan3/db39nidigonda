7b19a53be1856b213dac85e0c8c38c68c79db371
sandhyan3 <S545239@NWMISSOURI.EDU>
2021-11-12 12:12:37 -0600

    added

--- controllers/costume.js
+++ controllers/costume.js
@@ -59,3 +59,24 @@ exports.costume_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
 };
+
+// Handle Costume update form on PUT.
+exports.costume_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await Costume.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.costume_type)
+ toUpdate.costume_type = req.body.costume_type;
+ if(req.body.cost) toUpdate.cost = req.body.cost;
+ if(req.body.size) toUpdate.size = req.body.size;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+ }
+};
