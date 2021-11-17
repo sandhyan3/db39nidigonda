b08cbab2294bceb503e1074d13382c68352a188c
sandhyan3 <S545239@NWMISSOURI.EDU>
2021-11-16 23:47:35 -0600

    added

--- controllers/costume.js
+++ controllers/costume.js
@@ -80,3 +80,16 @@ ${JSON.stringify(req.body)}`)
 failed`);
  }
 };
+
+// Handle Costume delete on DELETE. 
+exports.costume_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await Costume.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+}; 
