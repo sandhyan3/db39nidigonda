51dbdb53f8bea5d9f3f1712382349a8449b8f9b3
sandhyan3 <S545239@NWMISSOURI.EDU>
2021-11-16 23:56:08 -0600

    added

--- routes/duck.js
+++ routes/duck.js
@@ -4,4 +4,7 @@ var router = express.Router();
 /* GET costumes */
 router.get('/', costume_controlers.costume_view_all_Page );
 router.get('/costumes/:id', costume_controlers.costume_detail);
+
+/* GET detail costume page */ 
+router.get('/detail', costume_controlers.costume_view_one_Page); 
 module.exports = router;
