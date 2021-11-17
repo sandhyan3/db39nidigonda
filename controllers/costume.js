fb8e91db8286116e4b1b82fac69df86d83acf4d4
sandhyan3 <S545239@NWMISSOURI.EDU>
2021-11-17 12:10:16 -0600

    added

--- controllers/costume.js
+++ controllers/costume.js
@@ -121,3 +121,18 @@ exports.costume_create_Page =  function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 };
+
+// Handle building the view for updating a costume. 
+// query provides the id 
+exports.costume_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Costume.findById(req.query.id) 
+        res.render('costumeupdate', { title: 'Costume Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+ 
